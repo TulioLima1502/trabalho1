455d5c1adbaefe191389b14000094dc13955e76e
TulioLima1502 <tulio1502@hotmail.com>
2018-05-23 19:37:00 -0300

    commit final oficial

--- trabalho1.cpp
+++ trabalho1.cpp
@@ -1958,7 +1958,7 @@ void segunda_passagem(string file_in, string file_out)
 		for (const auto &e : aux)
 			ofile << e << " ";
 		//TODO retirar linha abaixo depois
-		ofile << endl;
+		//ofile << endl;
 
 		++n_linha;
 		token_vector.clear();
@@ -2024,7 +2024,7 @@ int main(int argc, char *argv[])
 		//FUNÇOES DA MONTAGEM
 		string file_ = argv[3];
 		file_in = file_ + ".mcr";
-		string file_out = file_ + ".txt"; //todo trocar pra '.o'
+		string file_out = file_ + ".o"; //todo trocar pra '.o'
 		montagem(file_in, file_out);
 		//Realiza a montagem do código depois de expandir as macros
 	}

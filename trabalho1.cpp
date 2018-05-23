4c89b232d7581782a068f52752df02d8d073a3ab
dfsbora <dfsbora@gmail.com>
2018-05-23 10:36:40 -0300

    Gerando .o

--- trabalho1.cpp
+++ trabalho1.cpp
@@ -2037,7 +2037,7 @@ int main(int argc, char *argv[])
 		//FUNÇOES DA MONTAGEM
 		string file_ = argv[3];
 		file_in = file_ + ".mcr";
-		string file_out = file_ + ".txt"; //todo trocar pra '.o'
+		string file_out = file_ + ".o"; //todo trocar pra '.o'
 		montagem(file_in, file_out);
 		//Realiza a montagem do código depois de expandir as macros
 	}
